8c56ae25f8ae4512ad275546db5345494557e283
distilledx <alissxlace@proton.me>
2022-09-24 10:54:17 +0530

    restructuring

--- components/home_main.js
+++ components/home_main.js
@@ -3,7 +3,7 @@ import mainstyles from '../styles/mainstyles.module.css';
 export default function MainTitle() {
     return (
         <h6 className={mainstyles['main-head']}>
-            distilled<span className={mainstyles['main-head-x']}>X</span>
+            distilled<span style={{ color: 'red' }}>X</span>
         </h6>
     );
 }
