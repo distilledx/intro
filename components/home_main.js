b33028b242e4de9644d6ac430e579b6f04ef804f
distilledx <alissxlace@proton.me>
2022-09-30 22:02:43 +0530

    homepage changes

--- components/home_main.js
+++ components/home_main.js
@@ -5,6 +5,7 @@ import circles from '../styles/circles.module.css';
 export default function MainTitle() {
     const [textSize, setTextSize] = useState(0);
     const [bigCircleSize, setBigCircleSize] = useState(0);
+    const [smallCircleSize, setSmallCircleSize] = useState(0);
 
     useEffect(() => {
         if (window.outerWidth / 100 < 5) {
@@ -13,6 +14,7 @@ export default function MainTitle() {
         } else {
             setTextSize(window.outerWidth / 175);
             setBigCircleSize(30);
+            setSmallCircleSize(10);
         }
 
         const listener = window.addEventListener('resize', () => {
@@ -31,14 +33,15 @@ export default function MainTitle() {
     return (
         <>
             <div className={home_head['main']}>
-                <h6 className={home_head['main-head']} style={{ fontSize: `${textSize}rem` }}>
+                <h6 className={home_head['github']} style={{ fontSize: `${textSize}rem` }}>
                     distilled<span style={{ color: '#F4F4F9' }}>X</span>
                 </h6>
-                <h6 className={home_head['main-foot']} style={{ fontSize: `${textSize / 6}rem` }}>
-                    <span style={{ color: '#A5B6C0' }}>-By Tejas Vipin</span>
+                <h6 className={home_head['name']} style={{ fontSize: `${textSize / 6}rem` }}>
+                    By Tejas Vipin
                 </h6>
             </div>
-            <span className={circles['big']} style={{ height: `${bigCircleSize}vw`, width: `${bigCircleSize}vw` }}></span>
+            <span className={`${circles['circle']} ${circles['big']}`} style={{ height: `${bigCircleSize}vw`, width: `${bigCircleSize}vw` }}></span>
+            <span className={`${circles['circle']} ${circles['small']}`} style={{ height: `${smallCircleSize}vw`, width: `${smallCircleSize}vw` }}></span>
         </>
     );
 }
