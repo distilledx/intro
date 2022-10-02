7166f721921c55d759077b78dbfbb89ac162050d
distilledx <alissxlace@proton.me>
2022-10-02 20:39:33 +0530

    fixed scroll indicator

--- components/scroll.js
+++ components/scroll.js
@@ -2,16 +2,11 @@ import { useEffect, useState } from 'react';
 import scrollStyle from '../styles/scroll.module.css';
 
 export default function ScrollCount() {
-    const [scroll, setScroll] = useState(0);
-    const [height, setHeight] = useState(0);
     const [scrollHeight, setScrollHeight] = useState(0);
 
     useEffect(() => {
-        setHeight(document.documentElement.scrollTop - document.documentElement.clientHeight);
         const listener = window.addEventListener('scroll', () => {
-            setScroll(window.scrollY);
-            setScrollHeight((scroll / height) * 100);
-            console.log(`Height: ${height}, Scroll: ${scroll}, ScrollHeight: ${scrollHeight}`);
+            setScrollHeight(Math.floor((window.scrollY / window.scrollMaxY) * 100));
         });
 
         return () => window.removeEventListener('scroll', listener);
