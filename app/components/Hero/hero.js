3e2667e2b32a087d9f978c8383b20680d5842992
distilledx <alissxlace@proton.me>
2023-07-02 16:43:27 +0530

    Small setup

---
+++ app/components/Hero/hero.js
@@ -0,0 +1,10 @@
+import styles from "./hero.module.css";
+
+export default function Hero() {
+    return (
+        <>
+            <p className={styles.head}>DISTILLEDX</p>
+            <p>or Tejas Vipin</p>
+        </>
+    );
+}
