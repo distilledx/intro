02fe2b0ee3298aa1cd4f728ca634eeb844aed2b6
distilledx <alissxlace@proton.me>
2023-07-04 12:55:53 +0530

    added vw lines

--- app/components/Hero/hero.js
+++ app/components/Hero/hero.js
@@ -5,13 +5,17 @@ export default function Hero() {
         <>
             <div className={styles.text}>
                 <div className={styles.head}>
+                    <div className={styles.lineLeftH}>
+                        <div className={`${styles.arrow} ${styles.left}`}></div>
+                        <div className={styles.lineLeftV}></div>
+                    </div>
+                    <div className={styles.lineUpV}>
+                        <div className={`${styles.arrow} ${styles.down}`}></div>
+                        <div className={styles.lineUpH}></div>
+                    </div>
                     <p>DISTILLEDX</p>
                 </div>
                 <p className={styles.sub}>or Tejas Vipin</p>
-                <div className={styles.lineUpV}>
-                    <div className={`${styles.arrow} ${styles.down}`}></div>
-                    <div className={styles.lineUpH}></div>
-                </div>
             </div>
         </>
     );
