91fb98b4601636316471b77d39ba238a1481fd24
distilledx <alissxlace@proton.me>
2023-07-04 09:55:43 +0530

    added vh lines

--- app/components/Hero/hero.js
+++ app/components/Hero/hero.js
@@ -3,9 +3,15 @@ import styles from "./hero.module.css";
 export default function Hero() {
     return (
         <>
-            <div className={styles.center}>
-                <p className={styles.head}>DISTILLEDX</p>
+            <div className={styles.text}>
+                <div className={styles.head}>
+                    <p>DISTILLEDX</p>
+                </div>
                 <p className={styles.sub}>or Tejas Vipin</p>
+                <div className={styles.lineUpV}>
+                    <div className={`${styles.arrow} ${styles.down}`}></div>
+                    <div className={styles.lineUpH}></div>
+                </div>
             </div>
         </>
     );
