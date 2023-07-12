46a7c4e98dce78a61e764f3540fdc6199f93fe21
distilledx <alissxlace@proton.me>
2023-07-12 22:26:28 +0530

    added scroll

--- app/components/Hero/hero.js
+++ app/components/Hero/hero.js
@@ -3,6 +3,9 @@ import styles from "./hero.module.css";
 export default function Hero() {
     return (
         <>
+            <div className={styles.triangle}>
+                <p className={styles.menuText}>Menu</p>
+            </div>
             <div className={styles.text}>
                 <div className={styles.head}>
                     <div className={styles.lineLeftH}>
@@ -16,6 +19,12 @@ export default function Hero() {
                     <p>DISTILLEDX</p>
                 </div>
                 <p className={styles.sub}>or Tejas Vipin</p>
+            </div>
+            <div className={styles.scroll}>
+                <div className={styles.circle} style={{ opacity: 0.15 }}></div>
+                <div className={styles.circle} style={{ opacity: 0.45 }}></div>
+                <div className={styles.circle} style={{ opacity: 0.75 }}></div>
+                <p className={styles.scrollText}>SCROLL</p>
             </div>
         </>
     );
