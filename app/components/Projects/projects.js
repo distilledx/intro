5c7bbaa0db7b1e36b1c6b26721645ce89639f6cf
distilledx <alissxlace@proton.me>
2023-07-30 10:54:47 +0530

    more changes

--- app/components/Projects/projects.js
+++ app/components/Projects/projects.js
@@ -4,7 +4,6 @@ export default function Projects() {
     return (
         <>
             <div className={styles.mainContainer}>
-                <p>testing</p>
                 <div className={styles.pinkL}></div>
                 <div className={styles.blueR}></div>
             </div>
