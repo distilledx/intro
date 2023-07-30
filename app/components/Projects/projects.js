02306ec3e7f6feb6dc60ce5679db683a7fefa181
distilledx <alissxlace@proton.me>
2023-07-30 19:54:20 +0530

    added base of second page

--- app/components/Projects/projects.js
+++ app/components/Projects/projects.js
@@ -1,11 +1,35 @@
 import styles from "./projects.module.css";
+import Image from "next/image";
+import lscreen from "../../../public/left-screen.png";
+import rscreen from "../../../public/right-screen.png";
+
+import p1L from "../../../public/projects/1-L.png";
+import p1R from "../../../public/projects/1-R.png";
 
 export default function Projects() {
     return (
         <>
-            <div className={styles.mainContainer}>
-                <div className={styles.pinkL}></div>
-                <div className={styles.blueR}></div>
+            <div className={styles.fullContainer}>
+                <div className={styles.mainContainer}>
+                    <div className={styles.pinkL}></div>
+                    <div className={styles.blueR}></div>
+                </div>
+                <div className={styles.right}>
+                    <Image src={rscreen} alt="" />
+                    <Image
+                        src={p1R}
+                        alt=""
+                        className={`${styles.pictures} ${styles.right}`}
+                    />
+                </div>
+                <div className={styles.left}>
+                    <Image src={lscreen} alt="" />
+                    <Image
+                        src={p1L}
+                        alt=""
+                        className={`${styles.pictures} ${styles.left}`}
+                    />
+                </div>
             </div>
         </>
     );
